a2d45182142c72640d9aab511fadee818e36a89b
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2023-05-15 09:56:11 +0600

    add home insurance section

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,6 +7,8 @@ function ExpenseItem() {
       <div className="expense-item__description">
         <h2>Car Insurance</h2>
         <div className="expense-item__price">$267.34</div>
+        <h2>Home Insurance</h2>
+        <div className="expense-item__price">$260.34</div>
       </div>
     </div>
   );

d1ce321dcb6e88769d053d40e8892e944c881f01
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2023-05-17 09:13:20 +0600

    add props to populate data into the component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {title: 'Car Insurance', price: 34, date: new Date('2014', '7', '15')},
+    {title: 'House Insurance', price: 40, date: new Date('2014', '7', '15')},
+    {title: 'Health Insurance', price: 50, date: new Date('2014', '7', '15')}
+
+  ];
+  console.log(expenses[1]);
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} price={expenses[0].price} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} price={expenses[1].price} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} price={expenses[2].price} date={expenses[2].date}></ExpenseItem>
     </div>
   );
 }

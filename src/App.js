f0f4682e614aabb143a76edbf0cef2b923a8d23b
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2023-05-18 00:34:26 +0600

    spliting the component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+import NewExpense from "./components/NewExpense";
 
 function App() {
   const expenses = [
@@ -7,12 +8,10 @@ function App() {
     {title: 'Health Insurance', price: 50, date: new Date('2014', '7', '15')}
 
   ];
-  console.log(expenses[1]);
+
   return (
     <div>
-      <ExpenseItem title={expenses[0].title} price={expenses[0].price} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} price={expenses[1].price} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} price={expenses[2].price} date={expenses[2].date}></ExpenseItem>
+      <NewExpense expenses={expenses}/>
     </div>
   );
 }

eeff7d33c8150990361de1e5d10e682d178c6b0a
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2023-05-19 10:48:14 +0600

    work on props and composition in react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import ExpenseItem from "./components/ExpenseItem";
-import NewExpense from "./components/NewExpense";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
-    {title: 'Car Insurance', price: 34, date: new Date('2014', '7', '15')},
-    {title: 'House Insurance', price: 40, date: new Date('2014', '7', '15')},
-    {title: 'Health Insurance', price: 50, date: new Date('2014', '7', '15')}
-
+    { title: "Car Insurance", price: 34, date: new Date("2014", "7", "15") },
+    { title: "House Insurance", price: 40, date: new Date("2014", "7", "15") },
+    { title: "Health Insurance", price: 50, date: new Date("2014", "7", "15") },
   ];
 
   return (
     <div>
-      <NewExpense expenses={expenses}/>
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,29 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+export default function Expenses(props) {
+  const expenses = props.items;
+
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        price={expenses[0].price}
+      />
+
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        price={expenses[1].price}
+      />
+
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        price={expenses[2].price}
+      />
+    </Card>
+  );
+}

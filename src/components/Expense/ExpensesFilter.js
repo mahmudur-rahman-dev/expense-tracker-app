f57cb4aede938c27e372623af6ce995ef8ce99fe
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2023-06-20 17:18:56 +0600

    refactor expense filter

--- src/components/Expense/ExpensesFilter.js
+++ src/components/Expense/ExpensesFilter.js
@@ -7,9 +7,7 @@ const ExpensesFilter = (props) => {
   };
 
   return (
-
     <div className="expenses-filter">
-    
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select value={props.selected}onChange={selectHandler}>
